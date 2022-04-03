497993ca1a53b057b57e0762278adf096c971e16
Luis Fernando Domingo Gomez <al222010066@gmail.com>
2022-04-03 15:03:18 -0500

    Avance pdf

--- src/_components/Book.js
+++ src/_components/Book.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Card, Button } from 'react-bootstrap';
+import Pdf from './Pdf';
 
 export default class Book extends React.Component{
     state = {
@@ -17,27 +18,29 @@ export default class Book extends React.Component{
         const{books} = this.state
         return(
             <div className="container">
-            <br/>
-            <h2> Libros</h2>
-            <br/>
-            <br/>
-                {books.map((book, i) =>
-                    <Card key={i}>
-                            <center><Card className="card mb-3" style={{maxWidth: '170px'}}>
-                                <Card.Img variant="top" src={book.image} width="150px"/>
+                <br/>
+                <h2> Libros</h2>
+                <br/>
+                <br/>
+                <div className="row justify-content-center">
+                    {books.map((book, i) =>
+                        <Card key={i}>
+                            <Card className="col card mb-3" style={{maxWidth: '155px'}}>
+                                <Card.Img src={book.image} width="5rem"/>
                                 {/* style={{ width: '5rem' }} Se podria agregar este style al Img pero lo cols ya esta definiendo su ancho*/}
                                 <Card.Body>
-                                   <Card.Title>{book.name}</Card.Title>
-                                    <Card.Text>
-                                        {book.author_id}
-                                    </Card.Text>
-                                    <div className="row justify-content-around">
-                                        <Button href={book.pdf} class="btn btn-primary active" role="button" data-bs-toggle="button" aria-pressed="true">Leer</Button>
-                                    </div>
+                                    <Card.Title>{book.name}</Card.Title>
+                                        <Card.Text>
+                                            {book.author_id}
+                                        </Card.Text>
+                                        <div className="row justify-content-around">
+                                            <Button to={Pdf}>Leer</Button>
+                                        </div>
                                 </Card.Body>
-                            </Card></center>
-                    </Card>
-                )}
+                            </Card>
+                        </Card>
+                    )}
+                </div>
             </div>
         
         )
